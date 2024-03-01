aab20821c890bfbd0364786a8e11a0de3a2c6147
Sascha Höing <sascha.hoeing@ametek.com>
2024-03-01 10:31:13 +0100

    Add project files.

---
+++ VlcMemoryTest/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using LibVLCSharp.Shared;
+using System.Windows;
+
+namespace VlcMemoryTest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private MediaPlayer? m_MediaPlayer;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Initialize();
+		}
+
+		public void Initialize()
+		{
+			var arguments = new List<string>
+			{
+				// un-comment this line and memory consumption will rise
+				// $"--video-filter=croppadd{{cropleft=200,croptop=100,cropright=200,cropbottom=100}}"
+			};
+
+			var rtspArguments = new MediaConfiguration() { EnableHardwareDecoding = true }.Build();
+
+			var libVlc = new LibVLC(arguments.ToArray());
+			m_MediaPlayer = new MediaPlayer(libVlc);
+			var m_MediaStream = new Media(libVlc, $"file_example_MP4_640_3MG.mp4", FromType.FromPath, rtspArguments);
+			m_MediaPlayer.Media = m_MediaStream;
+			VideoView.MediaPlayer = m_MediaPlayer;
+			m_MediaStream.Dispose();
+		}
+
+		private void PlayButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (m_MediaPlayer != null)
+				ThreadPool.QueueUserWorkItem(_ => m_MediaPlayer.Play());
+		}
+
+		private void StopButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (m_MediaPlayer != null)
+				ThreadPool.QueueUserWorkItem(_ => m_MediaPlayer.Stop());
+		}
+	}
+}
